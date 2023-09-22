57fc22ed238d500237c0d078c21b9e7729ca8391
MutugiD <dennismutugi@hotmail.com>
2023-09-22 13:20:57 +0300

    project outline

---
+++ template.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+list_of_files = [
+    f"app/model/__init__.py",
+    f"app/model/machine_learning_pipeline.py", 
+    f"app/preprocessing/__init__.py",
+    f"app/preprocessing/nlp_preprocessing.py",
+    f"app/__init__.py",
+    f"app/main.py",
+    f"app/README.md",
+    f"model_dev/data/emotion-labels-test.csv", 
+    f"model_dev/data/emotion-labels-train.csv", 
+    f"model_dev/data/emotion-labels-vals.csv", 
+    f"model_dev/README.md", 
+    ".gitignore", 
+    "Dockerfile",
+    "README.md",
+    "build_model.py", 
+    "entry.sh",
+    "install_requirements.sh",
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
